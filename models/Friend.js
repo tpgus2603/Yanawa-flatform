ae11cf525ae54118c6350d54ac721ac66ff0d2f3
조대희 <joedaehui@ajou.ac.kr>
2024-11-15 23:00:00 +0900

    refactor: friend 모델 컬럼 수정 (#7)

--- models/Friend.js
+++ models/Friend.js
@@ -5,19 +5,20 @@ const sequelize = require('../config/sequelize');
 const User = require('./User');
 
 const Friend = sequelize.define('Friend', {
-  type: {
-    type: DataTypes.ENUM('NORMAL', 'SPECIAL'),
-    allowNull: false,
-  },
+    status: {
+        type: DataTypes.ENUM('PENDING', 'ACCEPTED'),
+        allowNull: false,
+        defaultValue: 'PENDING'
+    }
 }, {
-  tableName: 'Friends',
-  timestamps: true,
+    tableName: 'Friends',
+    timestamps: true,
 });
 
 Friend.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 Friend.belongsTo(User, { foreignKey: 'friend_id', as: 'friend' });
 
 User.hasMany(Friend, { foreignKey: 'user_id', as: 'friends' });
-User.hasMany(Friend, { foreignKey: 'friend_id', as: 'friendOf' });
+User.hasMany(Friend, { foreignKey: 'friend_id', as: 'friendRequests' });
 
 module.exports = Friend;
