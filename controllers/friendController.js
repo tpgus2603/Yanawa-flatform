da7b5243be825a69ac1e19001a84367e6479fc3d
조대희 <joedaehui@ajou.ac.kr>
2024-11-16 11:40:23 +0900

    feat: 받은 친구 목록 조회 컨트롤러 (#8)

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -25,6 +25,30 @@ class friendController {
             });
         }
     }
+
+    /**
+     * 받은 친구 요청 목록 조회
+     * GET /api/friend/requests/received
+     */
+    async getReceivedRequests(req, res) {
+        try {
+            const userId = req.user.id;
+            const requests = await FriendService.getReceivedRequests(userId);
+
+            return res.status(200).json({
+                success: true,
+                data: requests
+            });
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                error: {
+                    message: error.message,
+                    code: 'FETCH_ERROR'
+                }
+            });
+        }
+    }
 }
 
 module.exports = new friendController();
