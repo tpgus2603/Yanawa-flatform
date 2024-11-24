d22409fc8fce88ba748c57b3c7547876015ab12b
tpgus2603 <kakaneymar2424@gmail.com>
2024-11-25 01:47:20 +0900

    refactor: 미팅방 인원제한,현재인원 컬럼 추가 및 관련로직추가(#16)

--- models/meeting.js
+++ models/meeting.js
@@ -1,37 +1,53 @@
 // models/Meeting.js
 const { DataTypes } = require('sequelize');
-const sequelize  = require('../config/sequelize');
-const User = require('./user');
+const sequelize = require('../config/sequelize');
+const User = require('./User');
 
 const Meeting = sequelize.define('Meeting', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-  },
-  time_idx_start: { 
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  time_idx_end: { 
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  location: {
-    type: DataTypes.STRING,
-  },
-  time_idx_deadline: {
-    type: DataTypes.INTEGER,
-  },
-  type: {
-    type: DataTypes.ENUM('OPEN', 'CLOSE'),
-    allowNull: false,
-  },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+    },
+    time_idx_start: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    time_idx_end: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    location: {
+        type: DataTypes.STRING,
+    },
+    time_idx_deadline: {
+        type: DataTypes.INTEGER,
+    },
+    type: {
+        type: DataTypes.ENUM('OPEN', 'CLOSE'),
+        allowNull: false,
+        defaultValue: 'OPEN',
+    },
+    chatRoomId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    max_num: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10, // 기본값 설정 (필요에 따라 조정)
+    },
+    cur_num: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1, // 생성자 자신 포함
+    },
 }, {
-  tableName: 'Meetings',
-  timestamps: false,
+    tableName: 'Meetings',
+    timestamps: true,
+    underscored: true,
 });
 
 module.exports = Meeting;
