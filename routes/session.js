234d03a18dad66c65516a6ec6b8c6a4200d34768
Ubuntu <ubuntu@ip-172-31-1-27.ap-northeast-2.compute.internal>
2024-12-07 18:15:45 +0000

    refactor: 세션오류 수정

--- routes/session.js
+++ routes/session.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // GET /api/session/info
 router.get('/info', (req, res) => {
-  if (req.session && req.session.user) {
-    const { email, name } = req.session.user;
-
+  if (req.user) {
+    const { email, name } = req.user;
+  // 캐싱 비활성화
+    res.set('Cache-Control', 'no-store');
+    res.set('Pragma', 'no-cache');	  
     return res.status(200).json({
       user: {
         email,
@@ -13,8 +15,9 @@ router.get('/info', (req, res) => {
       },
     });
   }
-
   // 세션이 만료되었거나 사용자 정보가 없는 경우
+  res.set('Cache-Control', 'no-store');
+  res.set('Pragma', 'no-cache');
   res.status(401).json({
     message: '세션이 만료되었거나 사용자 정보가 없습니다.',
   });
